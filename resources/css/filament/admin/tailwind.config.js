5eb911908986ca3e7b007dba7807024eb008695e
riodewanto <rio.dewanto@meteor.id>
2024-03-04 22:16:19 +0700

    remove unused content

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -6,7 +6,6 @@ export default {
         "./app/Filament/**/*.php",
         "./resources/views/filament/**/*.blade.php",
         "./vendor/filament/**/*.blade.php",
-        "./vendor/awcodes/filament-tiptap-editor/resources/**/*.blade.php",
     ],
     theme: {
         extend: {
