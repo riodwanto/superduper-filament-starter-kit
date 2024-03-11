d8e1f5c30a1591f0dab86136aa8198e9a9812c68
riodewanto <rio.dewanto@meteor.id>
2024-03-11 20:15:15 +0700

    feat: forced bind model to policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,13 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use App\Models\Blog\Category as BlogPostCategory;
+use App\Models\Blog\Post as BlogPost;
 use App\Policies\ActivityPolicy;
+use App\Policies\Blog\CategoryPolicy as BlogPostCategoryPolicy;
+use App\Policies\Blog\PostPolicy as BlogPostPolicy;
+use App\Policies\ExceptionPolicy;
+use BezhanSalleh\FilamentExceptions\Models\Exception;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Spatie\Activitylog\Models\Activity;
 
@@ -16,7 +22,10 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Activity::class => ActivityPolicy::class
+        Activity::class => ActivityPolicy::class,
+        BlogPostCategory::class => BlogPostCategoryPolicy::class,
+        BlogPost::class => BlogPostPolicy::class,
+        Exception::class => ExceptionPolicy::class,
     ];
 
     /**
