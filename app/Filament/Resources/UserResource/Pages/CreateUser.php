fd53dddfb5b27b63f0bc3eab7f8f11c87fef5fe1
riodwanto <riodp.tech@gmail.com>
2024-04-18 20:10:14 +0700

    style: disable email verification for add user

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -21,25 +21,25 @@ class CreateUser extends CreateRecord
 
     protected function afterCreate(): void
     {
-        $user = $this->record;
-        $settings = app(MailSettings::class);
+        // $user = $this->record;
+        // $settings = app(MailSettings::class);
 
-        if (! method_exists($user, 'notify')) {
-            $userClass = $user::class;
+        // if (! method_exists($user, 'notify')) {
+        //     $userClass = $user::class;
 
-            throw new Exception("Model [{$userClass}] does not have a [notify()] method.");
-        }
+        //     throw new Exception("Model [{$userClass}] does not have a [notify()] method.");
+        // }
 
-        $notification = new VerifyEmail();
-        $notification->url = Filament::getVerifyEmailUrl($user);
+        // $notification = new VerifyEmail();
+        // $notification->url = Filament::getVerifyEmailUrl($user);
 
-        $settings->loadMailSettingsToConfig();
+        // $settings->loadMailSettingsToConfig();
 
-        $user->notify($notification);
+        // $user->notify($notification);
 
-        Notification::make()
-            ->title(__('resource.user.notifications.notification_resent.title'))
-            ->success()
-            ->send();
+        // Notification::make()
+        //     ->title(__('resource.user.notifications.notification_resent.title'))
+        //     ->success()
+        //     ->send();
     }
 }
