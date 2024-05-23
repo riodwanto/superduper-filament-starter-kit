dfc1d5e21d47bb7f0d8fc09ebfcb3fe48de9df0a
riodwanto <riodp.tech@gmail.com>
2024-05-23 12:28:57 +0700

    feat: form layout improvement

--- app/Filament/Resources/BannerResource.php
+++ app/Filament/Resources/BannerResource.php
@@ -23,41 +23,115 @@ class BannerResource extends Resource
     protected static ?int $navigationSort = -1;
     protected static ?string $navigationIcon = 'fluentui-image-shadow-24';
 
+    protected static function getLastSortValue(): int
+    {
+        return Banner::max('sort') ?? 0;
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('banner_category_id')
-                    ->relationship('category', 'name')
-                    ->searchable()
-                    ->required(),
-                Forms\Components\TextInput::make('sort')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('title')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->maxLength(500),
-                SpatieMediaLibraryFileUpload::make('media')
-                    ->collection('banners')
-                    ->multiple()
-                    ->reorderable()
-                    ->required(),
-                Forms\Components\Toggle::make('is_visible')
-                    ->required(),
-                Forms\Components\DateTimePicker::make('start_date'),
-                Forms\Components\DateTimePicker::make('end_date'),
-                Forms\Components\TextInput::make('click_url')
-                    ->maxLength(255),
-                Forms\Components\Select::make('click_url_target')
-                    ->options([
-                        '_blank' => 'New Tab',
-                        '_self' => 'Current Tab',
-                        // '_parent',
-                        // '_top'
+                Forms\Components\Tabs::make('Banner Details')
+                    ->tabs([
+                        Forms\Components\Tabs\Tab::make('General')
+                            ->icon('heroicon-o-information-circle')
+                            ->schema([
+                                Forms\Components\Section::make('Main Details')
+                                    ->description('Fill out the main details of the banner')
+                                    ->icon('heroicon-o-clipboard')
+                                    ->schema([
+                                        Forms\Components\Select::make('banner_category_id')
+                                            ->label('Category')
+                                            ->relationship('category', 'name')
+                                            ->searchable()
+                                            ->required(),
+                                        Forms\Components\Select::make('is_visible')
+                                            ->label('Is Visible')
+                                            ->default(1)
+                                            ->options([
+                                                0 => "No",
+                                                1 => "Yes",
+                                            ])
+                                            ->native(false)
+                                            ->required(),
+                                        Forms\Components\TextInput::make('title')
+                                            ->label('Title')
+                                            ->maxLength(255)
+                                            ->columnSpan(2),
+                                        Forms\Components\MarkdownEditor::make('description')
+                                            ->label('Description')
+                                            ->helperText('Provide a description for the banner')
+                                            ->maxLength(500)
+                                            ->columnSpanFull(),
+                                    ])
+                                    ->compact()
+                                    ->columns(2),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('Images')
+                            ->icon('heroicon-o-photo')
+                            ->schema([
+                                Forms\Components\Section::make('Image')
+                                    ->description('Upload banner images here')
+                                    ->schema([
+                                        SpatieMediaLibraryFileUpload::make('media')
+                                            ->hiddenLabel()
+                                            ->helperText('Select and upload images for the banner')
+                                            ->collection('banners')
+                                            ->multiple()
+                                            ->reorderable()
+                                            ->required(),
+                                    ])
+                                    ->compact(),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('Scheduling')
+                            ->icon('heroicon-o-calendar')
+                            ->schema([
+                                Forms\Components\Section::make('Schedule')
+                                    ->description('Set the scheduling details for the banner')
+                                    ->schema([
+                                        Forms\Components\DateTimePicker::make('start_date')
+                                            ->label('Start Date')
+                                            ->helperText('Select the start date and time'),
+                                        Forms\Components\DateTimePicker::make('end_date')
+                                            ->label('End Date')
+                                            ->helperText('Select the end date and time'),
+                                    ])
+                                    ->compact()
+                                    ->columns(2),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('Additional Settings')
+                            ->icon('heroicon-o-cog')
+                            ->schema([
+                                Forms\Components\Section::make('Settings')
+                                    ->description('Additional settings for the banner')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('sort')
+                                            ->label('Sort Order')
+                                            ->helperText('Set the sort order of the banner')
+                                            ->required()
+                                            ->numeric()
+                                            ->default(static::getLastSortValue() + 1),
+                                        Forms\Components\TextInput::make('click_url')
+                                            ->label('Click URL')
+                                            ->helperText('Enter the URL to navigate to when the banner is clicked')
+                                            ->default('#')
+                                            ->maxLength(255),
+                                        Forms\Components\Select::make('click_url_target')
+                                            ->label('Click URL Target')
+                                            ->helperText('Select how the URL should be opened')
+                                            ->options([
+                                                '_blank' => 'New Tab',
+                                                '_self' => 'Current Tab',
+                                                '_parent' => 'Parent Frame',
+                                                '_top' => 'Full Body of the Window'
+                                            ])
+                                            ->native(false),
+                                    ])
+                                    ->compact(),
+                            ]),
                     ])
-                    ->native(false),
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -70,7 +144,7 @@ class BannerResource extends Resource
                     ->wrap(),
                 Tables\Columns\TextColumn::make('title')
                     ->lineClamp(2)
-                    ->description(fn (Model $record): string => \Illuminate\Support\Str::limit($record->description, 50) ?? '')->wrap()
+                    ->description(fn(Model $record): string => \Illuminate\Support\Str::limit($record->description, 50) ?? '')->wrap()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('category.name')
                     ->searchable()
@@ -134,7 +208,7 @@ class BannerResource extends Resource
         return parent::getGlobalSearchEloquentQuery()->with(['category']);
     }
 
-    public static function getGlobalSearchResultTitle(Model $record): string | Htmlable
+    public static function getGlobalSearchResultTitle(Model $record): string|Htmlable
     {
         return $record->title;
     }
