fe3924a7b78f8bc552025fb8db39533ca4119b64
riodwanto <riodp.tech@gmail.com>
2024-09-14 08:10:03 +0700

    style: add new lang en notify user verification

--- lang/en/resource.php
+++ lang/en/resource.php
@@ -23,5 +23,9 @@ return [
     |--------------------------------------------------------------------------
     */
     'user.actions.resend_verification' => 'Resend Verification',
-    'user.notifications.notification_resent.title' => 'Email Verification Sent',
+    'user.notifications.verify_sent.title' => 'Email Verification Sent',
+    'user.notifications.verify_warning' => [
+        'title' => `Can't sent email verification!`,
+        'description' => `Please check your Mail Configuration or try again later.`
+    ],
 ];
