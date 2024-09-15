b22a712f2b6d1d78b14b8de576bdfa4bd8813e84
riodwanto <riodp.tech@gmail.com>
2024-09-15 08:43:30 +0700

    feat: add ApplicationInfo Widget

---
+++ app/Filament/Widgets/ApplicationInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class ApplicationInfo extends Widget
+{
+    protected static ?int $sort = -2;
+
+    protected static string $view = 'filament.widgets.application-info';
+}
