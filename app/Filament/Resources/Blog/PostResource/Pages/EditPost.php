2fb23566374c513aaf1712f902453b782afb9887
riodwanto <riodp.tech@gmail.com>
2024-09-16 07:04:32 +0700

    feat: add navigation record

--- app/Filament/Resources/Blog/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/Blog/PostResource/Pages/EditPost.php
@@ -5,15 +5,20 @@ namespace App\Filament\Resources\Blog\PostResource\Pages;
 use App\Filament\Resources\Blog\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use JoseEspinal\RecordNavigation\Traits\HasRecordNavigation;
 
 class EditPost extends EditRecord
 {
+    use HasRecordNavigation;
+
     protected static string $resource = PostResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [
+        $actions = [
             Actions\DeleteAction::make(),
         ];
+
+        return array_merge($this->getNavigationActions(), $actions);
     }
 }
