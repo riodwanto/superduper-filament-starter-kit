4e48068f29faef11f3984b35f8b1ae6039def9fd
aspirantzhang <admin@aspirantzhang.com>
2025-04-05 09:47:51 +0800

    fix: correct down method for migrations

--- database/migrations/2024_10_03_171809_create_folder_has_models_table.php
+++ database/migrations/2024_10_03_171809_create_folder_has_models_table.php
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('media_has_models');
+        Schema::dropIfExists('folder_has_models');
     }
 };
