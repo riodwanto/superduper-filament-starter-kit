943d9b96d2742d344c7ce500a99e86e31ef2dad8
riodwanto <riodp.tech@gmail.com>
2025-05-04 17:29:04 +0700

    style: reformat vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/admin/theme.css",
+            ],
             refresh: [
                 ...refreshPaths,
-                'app/Livewire/**',
+                "app/Livewire/**"
             ],
         }),
     ],
-})
+});
