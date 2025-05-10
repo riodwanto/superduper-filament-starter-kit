b9265183656452b9c03c398ef1498a87c608d95c
riodwanto <riodp.tech@gmail.com>
2025-05-10 12:15:39 +0700

    feat: update global tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
 
+/** @type {import('tailwindcss').Config} */
+
 export default {
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './resources/**/*.{php,js}',
+    ],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
     ],
 }
