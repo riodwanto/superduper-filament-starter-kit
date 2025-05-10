1774e631304a3139d60da551d63a97d9cedd8a8e
riodwanto <riodp.tech@gmail.com>
2025-05-10 12:16:47 +0700

    feat: update web route

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('components.superduper.pages.home');
+})->name('home');
+
+Route::get('/blog', function () {
+    return view('components.superduper.pages.coming-soon', ['page_type' => 'blog']);
+})->name('blog.index');
+
+Route::get('/blog/{blogId}', function ($blogId) {
+    return view('components.superduper.pages.coming-soon', ['page_type' => 'blog_post', 'id' => $blogId]);
+})->name('blog.show');
+
+Route::get('/contact-us', function () {
+    return view('components.superduper.pages.coming-soon', ['page_type' => 'contact']);
+})->name('contact-us');
+
+Route::get('/privacy-notice', function () {
+    return view('components.superduper.pages.coming-soon', ['page_type' => 'privacy']);
+})->name('privacy-notice');
+
+// A dedicated route for the generic coming soon page
+Route::get('/coming-soon', function () {
+    return view('components.superduper.pages.coming-soon', ['page_type' => 'generic']);
+})->name('coming-soon');
 
 Route::post('/contact', [App\Http\Controllers\ContactController::class, 'submit'])
     ->name('contact.submit');
 
-// TODO; Create actual blog preview component
-Route::post('/blog-preview')
-    ->name('blog.preview');
+// TODO: Create actual blog preview component
+Route::post('/blog-preview', function() {
+    // Implementation pending
+})->name('blog.preview');
