647253e57b3eb923ec15413eceb71dc052c1dc98
riodwanto <riodp.tech@gmail.com>
2025-05-10 12:15:20 +0700

    feat: publish config menu-builder

---
+++ config/filament-menu-builder.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'tables' => [
+        'menus' => 'menus',
+        'menu_items' => 'menu_items',
+        'menu_locations' => 'menu_locations',
+    ],
+];
